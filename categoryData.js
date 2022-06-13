9f75bb84bda34bc2387850fb061ac563083c61ce
ankitpanchal534 <ankitpanchal534@gmail.com>
2022-06-13 19:05:33 +0530

    update

---
+++ categoryData.js
@@ -0,0 +1,41 @@
+let categoryData = [
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/06d44453-7735-4ea2-a146-1fbd8a5bc48e1654766637157-Shop-By-Category_HP_02.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/1de91390-00d9-4de3-8640-a47686a60d9c1654766637166-Shop-By-Category_HP_03.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/8f307ca0-ae6b-4bc9-944e-c8447ab871f81654766637174-Shop-By-Category_HP_04.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/cbca4163-4855-485a-93be-3cbb8d62f5821654766637181-Shop-By-Category_HP_05.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/aa25955f-dbe0-41ee-9e75-af7ee9966c2d1654766637188-Shop-By-Category_HP_06.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/6b5c6054-aadb-4ac8-8bb5-5f7e54bc6ca51654766637195-Shop-By-Category_HP_07.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/64bd7936-8f81-416f-9f60-5ba6a749671f1654766637202-Shop-By-Category_HP_08.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/2a5ae5ad-12bb-4c79-823f-0ce08fffb14d1654766637209-Shop-By-Category_HP_09.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/f2af2965-6263-4c30-a5db-ec743fc82edd1654766637216-Shop-By-Category_HP_10.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/b279d44f-46d5-486f-9d22-c4f8d1122da61654766637223-Shop-By-Category_HP_11.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/f70c9363-588f-4682-b5e9-3676e433ce391654766637232-Shop-By-Category_HP_12.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/233ad199-63f5-4862-8c1a-8b203b4c8ebb1654766637239-Shop-By-Category_HP_13.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/2bb4c8b0-b961-4be2-8d92-8d20d15bfa0c1654766637247-Shop-By-Category_HP_14.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/d59e40c7-a8a4-4746-90c1-35545b37433e1654766637253-Shop-By-Category_HP_15.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/1e94196a-3a49-404f-9dbe-0f7b1d5566a21654766637259-Shop-By-Category_HP_16.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/57b3a627-7fa8-4093-91dd-7ed0e45256401654766637267-Shop-By-Category_HP_17.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/fdcc31ee-4b24-411a-84b5-d4f0c4a5c0e51654766637274-Shop-By-Category_HP_18.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/0e54ce81-5cfd-4ba0-a0ac-a230effd47211654766637282-Shop-By-Category_HP_19.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/6da88c6c-a7ce-4143-9304-a9ee122fafcb1654766637290-Shop-By-Category_HP_20.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/0630521f-d004-4e7b-97ee-d0f11f48c1451654766637301-Shop-By-Category_HP_21.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/6dfd6306-9021-4932-ba20-6a6380014c631654766637311-Shop-By-Category_HP_22.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/e0821297-389f-4bdc-a63a-cfaca163c0841654766637319-Shop-By-Category_HP_23.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/5c13a7f7-a78c-4edd-be35-6bd8265857901654766637330-Shop-By-Category_HP_24.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/ea30b785-5f7e-4f39-9f7d-62377e6736e71654766637339-Shop-By-Category_HP_25.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/4d4044e0-40a0-4791-a132-0df5b05044ef1654766637346-Shop-By-Category_HP_26.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/cd2fc2f5-e75e-4326-9786-3043e236769c1654766637353-Shop-By-Category_HP_27.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/de47d0a5-d37b-479b-bb23-fd850a5eab401654766637360-Shop-By-Category_HP_28.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/40c04f89-9244-4322-8a91-f4c9cc5e11981654766637370-Shop-By-Category_HP_29.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/8c4e96a6-3e40-461d-a3c0-4b397416ffba1654766637378-Shop-By-Category_HP_30.jpg",
+  "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/838227c6-8e05-4ff0-a6ee-6c32902015591654766637386-Shop-By-Category_HP_31.jpg",
+  "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/2bb43414-3f6a-4c77-9264-afab658b0a481654766637403-Shop-By-Category_HP_33.jpg",
+  "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/df646a25-73f7-4cbf-8c88-7093fed43a4b1654766637412-Shop-By-Category_HP_34.jpg",
+  "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/9/a1a85c11-4eae-4068-aa4b-d05cc2e5844a1654766637421-Shop-By-Category_HP_35.jpg",
+];
+
+const showcategoryData = categoryData.map((item, index) => {
+  return `<img src=${item} class="bestbyCategory-card" key=${index} />`;
+});
+//
+document.querySelector(".bestbyCategory").innerHTML = showcategoryData;
